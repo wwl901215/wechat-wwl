53a97a1023740375c6651b52fc14441339d03428
Wenliang Wang 王文亮 <wenliang.wang.o@nio.com>
2019-05-22 18:04:36 +0800

    add some picture

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,10 +1,28 @@
 Page({
-  data: {
-    list: []
-  },
-  onLoad: function () {
-    this.setData({
-      list: ["我的蔚来值", "我的订单", "在线支持", "加入蔚来", "设置"],
-    });
-  }
+    data: {
+        list: []
+    },
+    onLoad: function () {
+        this.setData({
+            list: [
+                {
+                    text: "我的蔚来值",
+                    type: 0,
+                    img: "../../images/me/b.png"
+                },
+                {
+                    text: "我的订单",
+                    type: 1,
+                    img: "../../images/me/e.png"
+                }, {
+                    text: "在线支持",
+                    type: 2, img: "../../images/me/f.png"
+                }, {
+                    text: "设置",
+                    type: 3,
+                    img: "../../images/me/i.png"
+                }
+            ],
+        });
+    }
 })
